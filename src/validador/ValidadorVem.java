e36b00eff6907ddcc15aa2ce9205e81558293524
Clarinhaserafim004 <Clarinhaserafim04@gmail.com>
2026-04-17 19:25:51 -0300

    commit

---
+++ src/validador/ValidadorVem.java
@@ -0,0 +1,33 @@
+package validador;
+
+public class ValidadorVem {
+    private double saldo;
+    private int numeroCartao;
+    private double tarifa;
+
+        public ValidadorVem(int numeroCartao, double saldo, double tarifa){
+            this.numeroCartao = numeroCartao;
+            this.saldo = saldo;
+            this.tarifa = tarifa;
+        }
+        public void pagarPassagem() {
+            if(saldo >= tarifa) {
+                saldo = saldo - tarifa;
+                System.out.println("PASSAGEM LIBERADA");
+            } else {
+                System.out.println("SALDO INSUFICIENTE");
+            }
+        }
+
+        public double getSaldo() {
+            return saldo;
+    }
+
+        public int getNumeroCartao() {
+            return numeroCartao;
+    }
+
+        public double getTarifa() {
+            return tarifa;
+    }
+}
